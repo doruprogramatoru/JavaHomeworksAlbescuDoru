079bdbfec702c2a4c29ceda94facf803622fb855
Doru <>
2017-01-30 23:30:42 +0200

    new commit

---
+++ src/javahomeworksalbescudoru/JavaHomeworksAlbescuDoru.java
@@ -0,0 +1,19 @@
+/*
+ * First project for homeworks.
+ */
+package javahomeworksalbescudoru;
+
+/**
+ *
+ * @author Doru
+ */
+public class JavaHomeworksAlbescuDoru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
