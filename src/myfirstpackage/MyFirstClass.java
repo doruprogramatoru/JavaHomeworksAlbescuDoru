452ef48dee85b15e4479b3977ad96f881dd1380f
Doru <Doru@hp>
2017-02-04 18:50:00 +0200

    My first Project

---
+++ src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,20 @@
+/*
+ * file of my first class written in Java
+ */
+package myfirstpackage;
+
+/**
+ * My first Java class
+ *
+ * @author Doru
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is the mother of oposum");
+    }
+    
+}
