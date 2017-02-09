ca7bac15f9084703468003e3e1066dda4188c887
Doru <Doru@hp>
2017-02-09 17:23:31 +0200

    Aceasta este tema: My first self made program in Java.

---
+++ src/MyfirstselfmadeprograminJava/MyFirstSelfMadeProgramInJava.java
@@ -0,0 +1,20 @@
+/*
+ * file for my first self made program in Java
+ */
+package MyfirstselfmadeprograminJava;
+
+/**
+ * My first self made program in Java
+ * 
+ * @author Doru
+ */
+public class MyFirstSelfMadeProgramInJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Moonlight effect on gum boots!");
+    }
+    
+}
