629f1cd4e1f170dea36a093b4825a2ed043bb75f
Doru <Doru@hp>
2017-02-10 12:46:13 +0200

    Am facut tema: My first self made program in Java.

--- src/MyfirstselfmadeprograminJava/MyFirstSelfMadeProgramInJava.java
+++ src/MyfirstselfmadeprograminJava/MyFirstSelfMadeProgramInJava.java
@@ -14,7 +14,7 @@ public class MyFirstSelfMadeProgramInJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Moonlight effect on gum boots!");
+        System.out.println("Moonlight effect on gum boots!");//sau Efectul razelor de luna asupra cizmelor de guma - suna mai bine in romaneste fata de greaca veche :)
     }
     
 }
