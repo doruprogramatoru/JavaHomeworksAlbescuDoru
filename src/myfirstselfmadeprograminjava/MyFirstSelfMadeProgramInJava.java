ae2275df45fcf646c4f088661871e021272f6e49
Doru <Doru@hp>
2017-02-23 14:48:31 +0200

    This is my commit for homework

--- src/myfirstselfmadeprograminjava/MyFirstSelfMadeProgramInJava.java
+++ src/myfirstselfmadeprograminjava/MyFirstSelfMadeProgramInJava.java
@@ -5,20 +5,21 @@ package myfirstselfmadeprograminjava;
 
 /**
  * My first self made program in Java
- * 
+ *
  * @author Doru
  */
 import java.util.Scanner;
+
 public class MyFirstSelfMadeProgramInJava {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    System.out.println("Enter your name: ");// Allows a person to enter his/her name 
-    Scanner scanner = new Scanner(System.in);
-    String name = scanner.nextLine();
-    System.out.println("Your name is " + name);//The program read the line of introduced characters 
+        System.out.println("Enter your name: ");// Allows a person to enter his/her name 
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Your name is " + name);//The program read the line of introduced characters 
     }
-    
+
 }
